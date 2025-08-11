024baa76c02c729180b63f702e1157f03908f752
aria-sharifnia <aria.sharifnia@gmail.com>
2025-08-10 21:08:02 -0600

    created the skills section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -24,11 +24,39 @@ export interface AboutSocialLink extends Struct.ComponentSchema {
   };
 }
 
+export interface SkillsSkillItem extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skill_items';
+  info: {
+    displayName: 'skillItem';
+    icon: 'apps';
+  };
+  attributes: {
+    icon: Schema.Attribute.Media<'images'>;
+    label: Schema.Attribute.String;
+    level: Schema.Attribute.Decimal;
+  };
+}
+
+export interface SkillsSkillsCategory extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills_categories';
+  info: {
+    displayName: 'skillsCategory';
+    icon: 'stack';
+  };
+  attributes: {
+    items: Schema.Attribute.Component<'skills.skill-item', true>;
+    palette: Schema.Attribute.Enumeration<['aqua', 'emerald', 'sky']>;
+    title: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'about.icon-text': AboutIconText;
       'about.social-link': AboutSocialLink;
+      'skills.skill-item': SkillsSkillItem;
+      'skills.skills-category': SkillsSkillsCategory;
     }
   }
 }
