a2026f478392198ad3018ece52b6e92aeda168da
aria-sharifnia <aria.sharifnia@gmail.com>
2025-08-27 11:16:29 -0600

    fixes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,45 @@
-module.exports = [
-  'strapi::logger',
+export default [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': [
+            "'self'",
+            'http://localhost:5173',
+            'http://localhost:3000',
+            'https://aria.binarybridges.ca',
+            /\.vercel\.app$/, 
+          ],
+        },
+      },
+    },
+  },
+
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://aria.binarybridges.ca',
+        /\.vercel\.app$/, 
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+      keepHeaderOnError: true,
+    },
+  },
+
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
